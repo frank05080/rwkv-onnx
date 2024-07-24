028ddfb944c01c2ddb249ffeac2efee581fefac0
guanzhong.chen <guanzhong.chen@horizon.cc>
2024-07-24 17:24:44 +0800

    add choice of dump input of submodel1

--- test_seq_save_inputs.py
+++ test_seq_save_inputs.py
@@ -1,6 +1,13 @@
 import numpy as np
 import onnxruntime as ort
 from scipy.special import softmax
+import os
+
+input_folder_name = 'dumped_inputs_submodel1'
+if not os.path.exists(input_folder_name):
+    os.makedirs(input_folder_name)
+index = 0
+SAVE_INPUT = True
 
 class Model:
     
@@ -18,39 +25,45 @@ class Model:
         
         for i in range(len(input_names1)-1):
             if "wkv" in input_names1[i+1]:
-                inputs1[input_names1[i+1]] = state2[i-25] # statei2 has shape (24,16,64,64)
+                inputs1[input_names1[i+1]] = state2[i-25]
             else:
-                inputs1[input_names1[i+1]] = state[i] # statei has shape [48, 1024]
+                inputs1[input_names1[i+1]] = state[i]
+
+        # Save each numpy array in inputs1 as a .bin file
+        if SAVE_INPUT:
+            global index
+            for name, array in inputs1.items():
+                type_folder = os.path.join(input_folder_name, name)
+                os.makedirs(type_folder, exist_ok=True)
+                file_path = os.path.join(type_folder, f"{name + str(index)}.bin")
+                index += 1
+                array.tofile(file_path)
 
         output_names1 = self.session1.get_outputs()
         output_names1 = [x.name for x in output_names1]
-        # Run inference on subgraph1
         results1 = self.session1.run(
             output_names1,
             inputs1
         )
-        # print(output_names1)
         subgraph1_out1 = results1[0]
         subgraph1_out2 = results1[1]
         subgraph1_state = results1[2:27]
         subgraph1_state2 = results1[-13:]
         
-        # # ---------- Load the subgraph2 model ------------
         input_names2 = self.session2.get_inputs()
         input_names2 = [x.name for x in input_names2]
         inputs2 = {}
         inputs2[input_names2[0]] = subgraph1_out1
         inputs2[input_names2[1]] = subgraph1_out2
 
-        for i in range(len(input_names2)-2): # minus matvec_1532_out and add_1480_out
+        for i in range(len(input_names2)-2): 
             if "wkv" in input_names2[i+2]:
-                inputs2[input_names2[i+2]] = state2[i-10] # statei2 has shape (24,16,64,64)
+                inputs2[input_names2[i+2]] = state2[i-10]
             else:
-                inputs2[input_names2[i+2]] = state[i+25] # statei has shape [48, 1024]
+                inputs2[input_names2[i+2]] = state[i+25]
 
         output_names2 = self.session2.get_outputs()
         output_names2 = [x.name for x in output_names2]
-        # print("output_names2:", output_names2)
         results2 = self.session2.run(
             output_names2,
             inputs2
@@ -70,9 +83,6 @@ def npsample(ozut, temp: float = 1.0, top_p_usual: float = 0.8) -> int:
             ozut = ozut.cpu().numpy()
         except:
             ozut = np.array(ozut)
-    # out[self.UNKNOWN_CHAR] = -float('Inf')
-    # out[self.UNKNOWN_CHAR] = -float('Inf')
-    # turn to float if is half and cpu
     probs = softmax(ozut, axis=-1)
 
     sorted_probs = np.sort(probs)[::-1]
@@ -104,7 +114,7 @@ for token in tqdm.tqdm(prompt[:-1]):
     logits, state, state2 = model.forward(token, state, state2)
 print("Loaded prompt.")
 
-for i in range(1000):
+for i in range(20):
     logits, state, state2 = model.forward(prompt[-1], state, state2)
     prompt = prompt+[npsample(logits)]
     print(tokenizer.decode(prompt[-1:]),end="", flush=True)
